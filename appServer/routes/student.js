f420043bd092b21b9eae3f6f11c4b17e6b581442
David <dawdiken@gmail.com>
2018-01-02 21:08:56 +0000

    first try

---
+++ appServer/routes/student.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var dao = require('../../dao/bSimpleDAO');
+
+/* GET all students listing. */
+router.get('/', function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    dao.getAll(function(result){
+        //console.log(result);
+        res.send(result);
+    })
+    //console.log("in students home");
+});
+
+router.get('/delete/:id', function(req,res,next){
+    var id = req.param("id");
+    dao.delete(id,function(result){
+        res.send("{success:true}");
+    })
+})
+router.get('/:id', function(req,res,next){
+    var id = req.param("id");
+    dao.findById(id,function(result){
+        res.send(result[0]);
+    })
+})
+router.post('/create', function(req,res,next){
+    var student = req.body;
+    dao.insert(student, function(result){
+        res.send("done");
+    })
+})
+router.post('/delete', function(req,res,next){
+    var student = req.body;
+    dao.delete(student, function(result){
+        res.send("done");
+    })
+})
+router.post('/update', function(req,res,next){
+    var student = req.body;
+    dao.update(student, function(result){
+        res.send("done");
+    })
+})
+
+module.exports = router;
